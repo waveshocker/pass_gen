fa3977c0aed86468b9691a9842eda97601ceff38
unknown <binhu10@gmail.com>
2020-02-05 21:51:28 -0500

    Initial check to see if length is numeric value

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  //Define password character length
+  var PassLen = prompt("Please input a numeric value between 8 and 128 for password length.");
+    
+  //Check if input is actually a number and between 8 and 128 value
+  while (isNaN(parseInt(PassLen))) {
+    PassLen = prompt("Please input a numeric Value between 8 and 128 for password length.");    
+  }
+
+  // while (parseInt(PassLen) < 8 || parseInt(PassLen) > 128) {
+  //   PassLen = prompt("Please input a numeric Value between 8 and 128 for password length.");
+  // }
+   
+
+
+  // var PassUpper = prompt("Please input Upper Case Letters to be used.");
+  // var PassLower = prompt("Please input Lower Case Letter to be used.");
+  // var PassNum = prompt("Please input numeric values to be used.");
+  // var PassSpec = prompt("Please input special characters to be used.")
 }
 
 // Add event listener to generate button
