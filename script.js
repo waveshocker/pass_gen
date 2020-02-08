dd79581532ff361ee77f082be577b2e3448bdbad
unknown <binhu10@gmail.com>
2020-02-07 22:09:48 -0500

    Final touches to randomize character selected and ensure one is picked and correct length of password

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function writePassword() {
 function generatePassword() {
   //Define password character length
   // temporary password
-  var tempass; 
+  var tempass = ''; 
+  var allc = '';
 
   PassLen = prompt("Please input a numeric Value between 8 and 128 for password length.");    
   
@@ -25,21 +26,25 @@ function generatePassword() {
 
   while ((PassUpper.toLowerCase() !== "y") && (PassUpper.toLowerCase() !== "n")) {
     var PassUpper = prompt("Please input y or n for use of Uppercase in Password.");
+    
   }
-
+  
   if (PassUpper.toLowerCase() === "y") {
     var upperchar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
+    tempass += upperchar[Math.floor(Math.random() * upperchar.length)];
+    allc = allc.concat(upperchar);
   }
 
   var PassLower = prompt("Please input y or n for use of Lowercase in Password.");
   
   while ((PassLower.toLowerCase() !== "y") && (PassLower.toLowerCase() !== "n")) {
-    var PassLower = prompt("Please input y or n for use of Lowercase in Password.");
+    var PassLower = prompt("Please input y or n for use of Lowercase in Password.");    
   }
 
   if (PassLower.toLowerCase() === "y") {
     var lowerchar = 'abcdefghijklmnopqrstuvwxyz';
+    tempass += lowerchar[Math.floor(Math.random() * lowerchar.length)];
+    allc = allc.concat(lowerchar);
   }
 
   var PassNum = prompt("Please input y or n for use of numeric values in Password.");
@@ -50,15 +55,28 @@ function generatePassword() {
 
   if (PassNum.toLowerCase() === "y") {
     var numchar = '0123456789';
+    tempass += numchar[Math.floor(Math.random() * numchar.length)];
+    allc = allc.concat(numchar);
   }
 
   var PassSpec = prompt("Please input y or n for use of special characters in Password.");
 
-  while ((PassNum.toLowerCase() !== "y") && (PassNum.toLowerCase() !== "n")) {
-    var PassNum = prompt("Please input y or n for use of special characters in Password.");  
+  while ((PassSpec.toLowerCase() !== "y") && (PassSpec.toLowerCase() !== "n")) {
+    var PassSpec = prompt("Please input y or n for use of special characters in Password.");  
   }
   
+  if (PassSpec.toLowerCase() === "y") {
+    var spechar = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    tempass += spechar[Math.floor(Math.random() * spechar.length)];
+    allc = allc.concat(spechar);
+  }
+  
+  
+  for (var i = tempass.length; i < PassLen; i++) {
+    tempass += allc[Math.floor(Math.random() * allc.length)];
+  }
 
+  return tempass;
 }
 
 // Add event listener to generate button
